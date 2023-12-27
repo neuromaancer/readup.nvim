f538676af2e8d58f3c2f055581ff6f1762e93a4a
Neuromaancer <alafate.abulimiti@gmail.com>
2023-12-27 21:35:28 +0100

    feat:basic usage.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # readup.nvim
-Neovim plugin quickly look up one plugin's README.md
+
+## WIP
+
+This is a plugin for me to learn lua and neovim plugin development, so everything is subject to change.
+
+## Introduction
+
+Neovim plugin quickly look up one plugin's `README.md` locally. It's useful when you want to look up a plugin's README.md without leaving your neovim.
+
+Currently, it only supports plugins installed by [lazy.nvim](https://github.com/folke/lazy.nvim).
+
+## Installation
+
+Lazy.nvim:
+
+```lua
+{
+    "neuromaancer/readup.nvim",
+    cmd = "Readup",
+    config = true
+}
+```
+
+## Usage
+
+Just run :Readup \<plugin_name>.
+
+## TODO
+
+- \[ \] Support other plugin managers.
+- \[ \] Get the plugin name from the current cursor.
+
+## License
+
+MIT
+
+```
+```
---
+++ lua/readup.lua
@@ -0,0 +1,80 @@
+local M = {}
+
+M.plugins_folder = vim.fn.stdpath("data") .. "/lazy"
+
+-- Updated parser function to handle both full and short plugin names
+local function parse_plugin_name(str)
+	if str:find("/") then
+		-- Extracts the plugin name from "author/plugin_name" format
+		local _, plugin_name = str:match("([^/]+)/([^/]+)")
+		return plugin_name
+	else
+		-- If only the plugin name is provided
+		return str
+	end
+end
+
+-- Function to get a list of installed plugins
+local function get_installed_plugins()
+	local plugins = {}
+	local p, err = io.popen("ls " .. M.plugins_folder)
+	if not p then
+		vim.notify(
+			"Failed to open plugins directory: " .. err,
+			vim.log.levels.ERROR
+		)
+		return plugins
+	end
+	for plugin in p:lines() do
+		table.insert(plugins, plugin)
+	end
+	p:close()
+	return plugins
+end
+
+M.readup = function(plugin_string)
+	local plugin_name = parse_plugin_name(plugin_string)
+	local plugin_path = M.plugins_folder .. "/" .. plugin_name
+	local readme_path = plugin_path .. "/README.md"
+
+	-- Check if README.md exists
+	local f = io.open(readme_path, "r")
+	if f ~= nil then
+		io.close(f)
+		-- Open README.md in a new buffer
+		vim.api.nvim_command("edit " .. readme_path)
+	else
+		vim.notify(
+			"README.md not found for " .. plugin_name,
+			vim.log.levels.ERROR
+		)
+	end
+end
+-- Function to get a list of installed plugins for autocomplete
+
+function M.complete_plugin_names(arg_lead, cmd_line, cursor_pos)
+	local plugins = get_installed_plugins()
+	local matches = {}
+	for _, plugin in ipairs(plugins) do
+		if plugin:find(arg_lead) == 1 then
+			table.insert(matches, plugin)
+		end
+	end
+	return matches
+end
+
+function M.setup_command()
+	vim.api.nvim_create_user_command("Readup", function(opts)
+		M.readup(opts.args)
+	end, {
+		nargs = 1,
+		complete = M.complete_plugin_names,
+	})
+end
+
+function M.setup()
+	-- Setup or initialization code for your plugin
+	M.setup_command()
+end
+
+return M
